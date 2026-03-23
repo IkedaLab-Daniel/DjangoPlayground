479460c779c71afd18cb1c31062595ff25085a5e
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-03-24 02:02:54 +0800

    Add token authentication and secret view to enhance security

--- authPractice/tokenbase/urls.py
+++ authPractice/tokenbase/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import CategoryView, MenuItemView, SingleMenuItemView
+from .views import CategoryView, MenuItemView, SingleMenuItemView, secret
+
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('category/', CategoryView.as_view()),
     path('menu/', MenuItemView.as_view()),
-    path('menu/<str:pk>', SingleMenuItemView.as_view())
+    path('menu/<str:pk>', SingleMenuItemView.as_view()),
+    path('secret/', secret),
+    path('api-token-auth/', obtain_auth_token)
 ]
--- authPractice/tokenbase/views.py
+++ authPractice/tokenbase/views.py
@@ -1,6 +1,13 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 from rest_framework import generics
 from .serializers import CategorySerializer, MenuItemSerializer
 from .models import Category, MenuItem
+
+# > Auth modules - function-based
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
 class CategoryView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
@@ -16,3 +23,9 @@ class MenuItemView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+# > Protect - function-based view
+@api_view()
+@permission_classes([IsAuthenticated])
+def secret(request):
+    return Response({"message": "Some Secret Message"})
