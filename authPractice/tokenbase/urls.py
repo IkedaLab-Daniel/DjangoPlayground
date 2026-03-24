030a2d0676d567818cea7a7b0066c52418b5bdd6
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-03-24 23:19:48 +0800

    Add manager view URL to tokenbase for enhanced access control

--- authPractice/tokenbase/urls.py
+++ authPractice/tokenbase/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CategoryView, MenuItemView, SingleMenuItemView, secret
+from .views import CategoryView, MenuItemView, SingleMenuItemView, secret, manager_view
 
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -8,5 +8,9 @@ urlpatterns = [
     path('menu/', MenuItemView.as_view()),
     path('menu/<str:pk>', SingleMenuItemView.as_view()),
     path('secret/', secret),
-    path('api-token-auth/', obtain_auth_token)
+    path('api-token-auth/', obtain_auth_token),
+    path('manager/', manager_view)
 ]
+
+# > John Doe: 16c68b453466946dc7f0fe6ae22a01879a42660c
+# > Jimmy Doe: f479feb8442d8d142951a07c1ab488189799ff11
